1668836d14771c96c370f05aa807da0e56f99fc5
Zarmeena Khan <zarmeenakhan262004@email.com>
2026-02-27 18:37:04 +0500

    Fix: Add process exit to end test job

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-if (require('./index.js')) {
-  console.log('App loads successfully');
-}
- 
+const app = require('./index.js');
+console.log("App loads successfully");
+// Foran exit karein taake CI workflow complete ho jaye
+process.exit(0); 
