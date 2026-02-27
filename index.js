f31c1054a52fa48978aaa8f5164de1e1192effeb
Zarmeena Khan <zarmeenakhan262004@email.com>
2026-02-27 17:48:14 +0500

    Test: Triggering final CI/CD workflow

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+
+// Verification comment for CI/CD deployment test
 app.get('/', (req, res) => res.send('Hello CI/CD!'));
+
 app.listen(3000, () => console.log('Server on port 3000'));
- 
